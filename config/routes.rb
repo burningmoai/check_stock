3615663e52e003c65253f4d2f3925833e6d6c1fa
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-12-09 15:27:49 +0000

    [Add]ルーティングの記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "homes#top"
+  get 'about'=>'homes#about'
+  resources :foods do
+    resource :likes, only: [:create, :destroy]
+    resources :lists, except: [:show]
+  end
+  resources :users, only: [:show, :edit, :update]
+  resources :stocks
+  resources :categories, except: [:show, :new]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root to: "homes#top"
+ 
 end
