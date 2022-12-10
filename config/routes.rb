882de123fcee22da8a469f51769c5d7621035c35
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-12-10 14:53:16 +0000

    [Add]アバウトページ仮完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "stocks#index"
   get 'about'=>'homes#about'
-  get 'food_limit'=>'foods#limit'
+  get 'food_limit'=>'stocks#limit'
   resources :foods do
     resource :likes, only: [:create, :destroy]
-    resources :lists, except: [:show]
   end
+  resources :lists, except: [:show]
   resources :users, only: [:show, :edit, :update]
   resources :stocks
   resources :categories, except: [:show, :new]
