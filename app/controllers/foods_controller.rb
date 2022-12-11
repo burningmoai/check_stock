30d27074b56a5d999d49d5f52e963acd634289c9
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-12-11 11:51:37 +0000

    [Add]食材とカテゴリを登録・削除できるようになった

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,30 @@
 class FoodsController < ApplicationController
   def index
+    @food = Food.new
+    @foods = Food.all
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    @food.save
+      redirect_to request.referer,notice:"食材を登録しました!"
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    redirect_to request.referer, notice: "食材を削除しました"
   end
 
   def show
   end
 
   def edit
+  end
+private
+  def food_params
+    params.require(:food).permit(:name, :user_id, :category_id)
   end
 end
