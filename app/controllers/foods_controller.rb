ac481e84255119339bc3fcb1197798446f095a69
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-12-13 10:34:59 +0000

    [Add]ゲストログイン機能の追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,37 @@
 class FoodsController < ApplicationController
   def index
+    @food = Food.new
+    @foods = Food.all
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    @food.save
+      redirect_to request.referer,notice:"食材を登録しました!"
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    redirect_to request.referer, notice: "食材を削除しました"
   end
 
   def show
+    @food = Food.find(params[:id])
   end
 
   def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    food = Food.find(params[:id])
+    food.update(food_params)
+    redirect_to food_path, notice: "編集が完了しました"
+  end
+private
+  def food_params
+    params.require(:food).permit(:name, :user_id, :category_id)
   end
 end
